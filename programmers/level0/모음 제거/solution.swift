6f28c10e4fafa6e8f41f737f94e47d640a379135
Bueong <pcj0308@gmail.com>
2023-02-08 23:04:30 +0900

    programmers 모음 제거

---
+++ programmers/level0/모음 제거/solution.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+func solution(_ my_string:String) -> String {
+    var result = ""
+    result = my_string.components(separatedBy: ["a","e","i","o","u"]).joined()
+    return result
+}
