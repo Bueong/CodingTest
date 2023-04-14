292d63904258837849e5dcbbf0b50f82a5db90ae
OwlCJ <pcj0308@gmail.com>
2023-04-15 01:38:24 +0900

    programmers 숨어있는 숫자의 덧셈 (1)

---
+++ programmers/level0/숨어있는 숫자의 덧셈 (1)/solution.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ my_string:String) -> Int {
+    var result = 0
+    
+    for i in my_string {
+        if let num: Int = i.wholeNumberValue {
+            result += num
+        }
+    }
+    
+    return result
+}
