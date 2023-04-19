edf3a33f5301c3b7cb872865d88093218cf0d81c
OwlCJ <pcj0308@gmail.com>
2023-04-20 01:39:17 +0900

    programmers 분수의 덧셈

---
+++ programmers/level0/분수의 덧셈/solution.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+    if b == 0 {
+        return a
+    }
+    return gcd(b, a % b)
+}
+
+func solution(_ numer1:Int, _ denom1:Int, _ numer2:Int, _ denom2:Int) -> [Int] {
+    var result = [0, 0]
+    
+    if denom1 != denom2 {
+        result[1] = denom1 * denom2
+        result[0] = numer1 * denom2 + numer2 * denom1
+    } else {
+        result[1] = denom1
+        result[0] = numer1 + numer2
+    }
+    
+    let gcdNum = gcd(result[0], result[1])
+    
+    return result.map { $0 / gcdNum }
+}
+
