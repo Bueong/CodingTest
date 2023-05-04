554c24d82ad1820a095eb7f47cfa5e9c5c2c0b79
OwlCJ <pcj0308@gmail.com>
2023-05-05 08:20:46 +0900

    programmers 배열의 길이에 따라 연산

---
+++ programmers/level0/배열의 길이에 따라 다른 연산하기/solution.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+func solution(_ arr:[Int], _ n:Int) -> [Int] {
+    var evenResult = arr.enumerated().map { $0.offset % 2 == 0 ? $0.element + n : $0.element }
+    var oddResult = arr.enumerated().map { $0.offset % 2 == 1 ? $0.element + n : $0.element }
+    return arr.count % 2 == 1 ? evenResult : oddResult
+}
