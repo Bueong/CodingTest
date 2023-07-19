07f5abfe425e7eca6e6763ef3ac8cc698594b523
OwlCJ <pcj0308@gmail.com>
2023-07-19 18:32:21 +0900

    programmers 크레인 인형뽑기

---
+++ programmers/2019_카카오_개발자_겨울_인턴쉽/크레인 인형뽑기 게임/solution.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+func solution(_ board:[[Int]], _ moves:[Int]) -> Int {
+    var mutableBoard = board
+    var resultStack: [Int] = []
+    var result = 0
+    
+    for i in moves {
+        for j in 0..<board.count {
+            guard mutableBoard[j][i-1] != 0 else { continue }
+            
+            if !resultStack.isEmpty && resultStack.last == mutableBoard[j][i-1] {
+                resultStack.popLast()
+                mutableBoard[j][i-1] = 0
+                result += 2
+                break
+            } else {
+                resultStack.append(mutableBoard[j][i-1])
+                mutableBoard[j][i-1] = 0
+                break
+            }
+        }
+    }
+    return result
+}
