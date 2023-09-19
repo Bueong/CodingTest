9eb25d5fb8ac7a952579223292f53ebea190afbe
OwlCJ <pcj0308@gmail.com>
2023-09-19 23:10:04 +0900

    로그인 성공 코드 수정

--- programmers/level0/로그인 성공?/solution.swift
+++ programmers/level0/로그인 성공?/solution.swift
@@ -2,6 +2,7 @@ import Foundation
 
 func solution(_ id_pw:[String], _ db:[[String]]) -> String {
     var result = "fail"
+    
     for i in db {
         if id_pw[0] == i[0] {
             if id_pw[1] == i[1] {
