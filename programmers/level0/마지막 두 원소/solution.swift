34c7ee9c6f7e89c5a2b143aa6d89d85ffb0d8f3f
OwlCJ <pcj0308@gmail.com>
2023-09-20 22:11:04 +0900

    마지막 두 원소 코드 수정

--- programmers/level0/마지막 두 원소/solution.swift
+++ programmers/level0/마지막 두 원소/solution.swift
@@ -6,5 +6,6 @@ func solution(_ num_list:[Int]) -> [Int] {
         result[result.count - 1] > result[result.count - 2] ? result.last! - result[result.count - 2] : result.last! * 2
     }
     result.append(lastNum)
+    
     return result
 }
