5689a7dfec1f35eb190ae63ec9e3741b031dd5cf
OwlCJ <pcj0308@gmail.com>
2023-09-23 20:52:48 +0900

    모음 제거 코드 수정

--- programmers/level0/모음 제거/solution.swift
+++ programmers/level0/모음 제거/solution.swift
@@ -3,5 +3,6 @@ import Foundation
 func solution(_ my_string:String) -> String {
     var result = ""
     result = my_string.components(separatedBy: ["a","e","i","o","u"]).joined()
+    
     return result
 }
