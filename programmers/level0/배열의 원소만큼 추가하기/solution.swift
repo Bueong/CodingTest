0afe13e8b74a6930fbaeaffed4bf083ab2cc79d0
OwlCJ <pcj0308@gmail.com>
2023-09-28 10:38:19 +0900

    배열의 원소만큼 추가하기 컨벤션 수정

--- programmers/level0/배열의 원소만큼 추가하기/solution.swift
+++ programmers/level0/배열의 원소만큼 추가하기/solution.swift
@@ -2,10 +2,12 @@ import Foundation
 
 func solution(_ arr:[Int]) -> [Int] {
     var result: [Int] = []
+    
     for element in arr {
         for repeats in 0..<element {
             result.append(element)
         }
     }
+
     return result
 }
